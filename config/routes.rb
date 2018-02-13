85e85ad7e05be59e0648b43c49034c53f8746db7
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-13 15:13:12 +0100

    cest moche mais ca marche

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get    "tasks",          to: "tasks#index"
+  root :to => "tasks#index"
+
+  get    "tasks",          to: "tasks#index", as: :tasks
 
   get    "tasks/new",      to: "tasks#new"
-  post   "tasks",          to: "tasks#create"
 
-  get    "tasks/:id",      to: "tasks#show", as: :show
+  post   "tasks",          to: "tasks#create", as: :create
 
-  get    "tasks/:id/edit", to: "tasks#edit"
-  patch  "tasks/:id",      to: "tasks#update"
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit
+  get    "tasks/:id",      to: "tasks#show", as: :task
+
+  patch  "tasks/:id",      to: "tasks#update", as: :update
 
   delete "tasks/:id",      to: "tasks#destroy"
-
-  root :to => "tasks#index"
 end
